1e613f823f15b13bf0d4f282f850a4f8a063bb81
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-15 17:48:39 +0300

    chore: Инициализация проекта

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.5"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "io.github.zapolyarnydev"
+version = "0.0.1-SNAPSHOT"
+description = "Demo project for Spring Boot"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	runtimeOnly("org.postgresql:postgresql")
+
+	compileOnly("org.mapstruct:mapstruct:1.5.5.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.5.Final")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/java/io/github/zapolyarnydev/ptknow/PTKnowApplication.java
@@ -0,0 +1,13 @@
+package io.github.zapolyarnydev.ptknow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PTKnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PTKnowApplication.class, args);
+	}
+
+}
