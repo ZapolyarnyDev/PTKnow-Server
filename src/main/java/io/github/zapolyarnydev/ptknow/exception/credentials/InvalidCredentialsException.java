1c7212e1466c1944181a1fa5b84fc4078dd99032
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-16 14:10:17 +0300

    feat: добавлены исключения, возникающие при регистрации / авторизации

---
+++ src/main/java/io/github/zapolyarnydev/ptknow/exception/credentials/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package io.github.zapolyarnydev.ptknow.exception.credentials;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/io/github/zapolyarnydev/ptknow/exception/email/EmailAlreadyUsedException.java
@@ -0,0 +1,7 @@
+package io.github.zapolyarnydev.ptknow.exception.email;
+
+public class EmailAlreadyUsedException extends RuntimeException {
+    public EmailAlreadyUsedException(String email) {
+        super(String.format("Почта %s уже используется", email));
+    }
+}
