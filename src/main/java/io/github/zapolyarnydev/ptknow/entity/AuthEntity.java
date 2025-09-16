183e73c26381c8743fc595c0f2c2eefa40b43546
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-16 14:09:24 +0300

    feat: сущность и репозиторий авторизованного пользователя

---
+++ src/main/java/io/github/zapolyarnydev/ptknow/entity/AuthEntity.java
@@ -0,0 +1,44 @@
+package io.github.zapolyarnydev.ptknow.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "auth_data")
+@Getter
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AuthEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, unique = true, nullable = false)
+    UUID id;
+
+    @Column(updatable = false, unique = true, nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    @Setter
+    String password;
+
+    @Column(nullable = false, updatable = false)
+    Instant registeredAt;
+
+    @Builder
+    public AuthEntity(String email, String password, Instant registeredAt) {
+        this.registeredAt = registeredAt;
+        this.password = password;
+        this.email = email;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if(registeredAt == null)
+            registeredAt = Instant.now();
+    }
+}
---
+++ src/main/java/io/github/zapolyarnydev/ptknow/repository/AuthRepository.java
@@ -0,0 +1,16 @@
+package io.github.zapolyarnydev.ptknow.repository;
+
+import io.github.zapolyarnydev.ptknow.entity.AuthEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AuthRepository extends JpaRepository<AuthEntity, UUID> {
+
+    Optional<AuthEntity> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
