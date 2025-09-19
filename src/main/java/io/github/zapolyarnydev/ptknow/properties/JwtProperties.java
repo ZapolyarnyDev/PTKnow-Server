b051a495a81f203aeff7b62ed4d6b2baa35dd35c
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-19 20:58:21 +0300

    refactor: добавлено конфигурирование эмитента JWT токена

--- src/main/java/io/github/zapolyarnydev/ptknow/properties/JwtProperties.java
+++ src/main/java/io/github/zapolyarnydev/ptknow/properties/JwtProperties.java
@@ -1,6 +1,5 @@
 package io.github.zapolyarnydev.ptknow.properties;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -12,17 +11,20 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
 
 @Configuration
 @ConfigurationProperties(prefix = "security.jwt")
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class JwtProperties {
+
     @Getter
     @NotBlank
-    String secretKey;
+    String issuer;
+
+    @Getter
+    @NotBlank
+    String key;
 
     @NotNull
     Duration accessTokenExpiration = Duration.ofMinutes(15);
