482a009960386ec2b47efc3d3d11125d2203972d
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-19 18:17:35 +0300

    feat: Добавлена настройка безопасности для приложения; установлены базовые jwt энкодеры и декодеры

---
+++ src/main/java/io/github/zapolyarnydev/ptknow/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package io.github.zapolyarnydev.ptknow.config;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtDecoder jwtDecoder) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/register", "/auth/login", "/auth/refresh", "/oauth2/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth -> oauth
+                        .defaultSuccessUrl("/auth/oauth2/success", true)
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.decoder(jwtDecoder))
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder(@Value("${security.jwt}") String key) {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(key.getBytes()));
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(@Value("${security.jwt}") String key) {
+        return NimbusJwtDecoder.withSecretKey(new SecretKeySpec(key.getBytes(), "HmacSHA256")).build();
+    }
+}
