6fa716a270e30c6a5a583f49291b4398cfa18d7f
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-23 13:44:27 +0300

    feat(auth): реализация сервиса работы с авторизацией

---
+++ src/main/java/io/github/zapolyarnydev/ptknow/service/AuthService.java
@@ -0,0 +1,61 @@
+package io.github.zapolyarnydev.ptknow.service;
+
+import io.github.zapolyarnydev.ptknow.entity.user.UserEntity;
+import io.github.zapolyarnydev.ptknow.exception.email.EmailAlreadyUsedException;
+import io.github.zapolyarnydev.ptknow.exception.email.EmailNotFoundException;
+import io.github.zapolyarnydev.ptknow.exception.credentials.InvalidCredentialsException;
+import io.github.zapolyarnydev.ptknow.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthService implements UserDetailsService {
+
+    UserRepository repository;
+    PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UserEntity register(String fullName, String email, String password) {
+        if(repository.existsByEmail(email))
+            throw new EmailAlreadyUsedException(email);
+
+        String hashedPassword = passwordEncoder.encode(password);
+
+        var entity = UserEntity.builder()
+                .fullName(fullName)
+                .email(email)
+                .password(hashedPassword)
+                .build();
+
+        repository.save(entity);
+        log.info("User registered successfully. Full name: {}; Email: {}", fullName, email);
+        return entity;
+    }
+
+    @Transactional
+    public UserEntity authenticate(String email, String password) {
+        var entity = loadUserByUsername(email);
+
+        if(!passwordEncoder.matches(password, entity.getPassword()))
+            throw new InvalidCredentialsException("Неверный логин или пароль");
+
+        log.info("User log in successfully. Email: {}", email);
+        return entity;
+    }
+
+    @Override
+    public UserEntity loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username)
+                .orElseThrow(() -> new EmailNotFoundException(username));
+    }
+}
