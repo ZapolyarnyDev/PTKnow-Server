6afe7da09bfaa136351f71438f5328df1b1f5470
ZapolyarnyDev <zapolyarnynorth@gmail.com>
2025-09-24 19:47:48 +0300

    feat(API): стандартизация формата ответов

---
+++ src/main/java/io/github/zapolyarnydev/ptknow/api/v0/ApiResponse.java
@@ -0,0 +1,13 @@
+package io.github.zapolyarnydev.ptknow.api.v0;
+
+public record ApiResponse <T> (ApiStatus status, String message, T data) {
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(ApiStatus.SUCCESS, message, data);
+    }
+
+    public static <T> ApiResponse<T> failure(String message) {
+        return new ApiResponse<>(ApiStatus.FAILURE, message, null);
+    }
+
+}
---
+++ src/main/java/io/github/zapolyarnydev/ptknow/api/v0/ApiStatus.java
@@ -0,0 +1,8 @@
+package io.github.zapolyarnydev.ptknow.api.v0;
+
+public enum ApiStatus {
+
+    SUCCESS,
+    FAILURE;
+
+}
